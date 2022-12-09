e816f435e42371f3fa89bf55d50f9c832b7fdc56
wass <wassim.filali@gmail.com>
2022-12-09 18:12:18 +0100

    functional SSE

--- src/pages/api/events.js
+++ src/pages/api/events.js
@@ -1,19 +1,31 @@
 import {logger} from '../../libs/logger.js'
 
-let responses = []
-
-function remove(item,array){
-    const index = array.indexOf(item)
-    if(index > -1){
-        array.splice(index,1)
-    }
-}
+let counter = 0
 
 export async function get({request}){
 
     logger.info("api/events> get()")
-    const data = `data: ${JSON.stringify({message:"First"})}\n\n`;
-    const response = new Response(data, {
+    
+    //const data = `data: ${JSON.stringify({message:"First"})}\n\n`;
+    let timerId = undefined
+    const body = new ReadableStream({
+        start(controller){
+            timerId = setInterval(()=>{
+                //const data = new TextEncoder().encode("data: hello\r\n\r\n")
+                const data = `data: ${JSON.stringify({counter:counter})}\r\n\r\n`;
+                counter += 1
+                console.log(`Interval> ${counter}`)
+                controller.enqueue(data)
+            }, 5000)
+        },
+        cancel(){
+            if(typeof timerId === "number"){
+                clearInterval(timerId)
+            }
+        }
+    })
+
+    return new Response(body, {
         status: 200,
         headers: {
             'Content-Type': 'text/event-stream',
@@ -21,16 +33,4 @@ export async function get({request}){
             'Cache-Control': 'no-cache'          
         }
     });
-    //request.on('close',()=>{
-    //    remove(response,responses)
-    //})
-    //responses.push(response)
-    return response;
-    //keep getting error on return
 }
-
-//setInterval(()=>{
-//    responses.forEach((response)=>{
-//        response.write(JSON.stringify({message:"hi"}))
-//    })
-//},5000)
