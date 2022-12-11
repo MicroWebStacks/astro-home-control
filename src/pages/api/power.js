c25cc537f89a553853f3d9d0c97fd31d7f225f9f
wass <wassim.filali@gmail.com>
2022-12-11 13:00:33 +0100

    removed power redundant state

--- src/pages/api/power.js
+++ src/pages/api/power.js
@@ -1,45 +1,14 @@
-import * as mqtt from '../../libs/mqtt.js'
-import {delay} from '../../libs/utils'
-import {logger} from '../../libs/logger.js'
+import {delay} from '@/libs/utils'
+import {logger} from '@/libs/logger.js'
+import  {get_devices} from '@/libs/power_state'
 
-
-const devices = {
-  poster:{
-      topic:"lzig/poster socket",
-      control:"lzig/poster socket/set",
-      media_on:"/media/poster.png",
-      media_off:"/media/poster-dark.png",
-      state:"off",power:0,disabled:false
-  },
-  lifx:{
-      topic:"lzig/lifx socket",
-      control:"lzig/lifx socket/set",
-      media_on:"/media/lifx.png",
-      media_off:"/media/lifx-dark.png",
-      state:"off",power:0,disabled:false
-  },
-  mesh:{
-      topic:"lzig/wifi mesh socket",
-      control:"lzig/wifi mesh socket/set",
-      media_on:"/media/wifi-on.png",
-      media_off:"/media/wifi-off.png",
-      state:"off",power:0,disabled:false
-  },
-  pc:{
-      topic:"lzig/pc socket",
-      control:"lzig/pc socket/set",
-      media_on:"/media/pc.png",
-      media_off:"/media/pc.png",
-      state:"off",power:0,disabled:true
-  },
-}
-
-const devices_list = ["lifx","mesh","poster","pc"]
 
 export async function put({request}){
   logger.info(`api/power> put()`)
   const content = await request.json()
   const device = content.name
+  const devices = get_devices()
+  const devices_list = Object.keys(devices)
     if(!devices_list.includes(device)){
         logger.error(`api/power> no '${device}' device available for control`)
         return new Response(JSON.stringify({state:"off"}), {
@@ -73,6 +42,7 @@ export async function put({request}){
 
 export async function get({params}){
 
+  const devices = get_devices()
   const device = params.device
   if(!Object.keys(devices).includes(device)){
       logger.error(`api/power> device : '${device}' not available`)
@@ -83,10 +53,10 @@ export async function get({params}){
           "Content-Type": "application/json"
         }
       });        
-}
+  }
 
-logger.verbose(`api/power> get() ${device} = ${devices[device].state}`)
-return new Response(JSON.stringify({state:devices[device].state}), {
+  logger.verbose(`api/power> get() ${device} = ${devices[device].state}`)
+  return new Response(JSON.stringify({state:devices[device].state}), {
       status: 200,
       headers: {
         "Content-Type": "application/json"
@@ -94,18 +64,4 @@ return new Response(JSON.stringify({state:devices[device].state}), {
     });    
 }
 
-mqtt.Emitter.on('power',(data)=>{
-  try{
-    for (const [name, value] of Object.entries(devices)) {
-      if(data.topic == value.topic){
-        value.state = JSON.parse(data.msg).state
-        logger.debug(`api/power> ${name} updated to ${value.state}`)
-      }
-    }
-  }catch(e){
-    logger.info(`Handling all exceptions : ${e.message}`)
-  }
-})
-
 logger.info("api/power> init")
-mqtt.start()
